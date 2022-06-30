df5791bdc853700218cd9ebaa9ba63894fc1337e
ARPITxxPATEL <arpitkumar2105@gmail.com>
2022-07-01 00:06:31 +0530

    init commit

---
+++ helper-hardhat-config.js
@@ -0,0 +1,27 @@
+const { ethers } = require("hardhat")
+
+const networkConfig = {
+    4: {
+        name: "rinkeby",
+        vrfCoordinatorV2: "0x271682DEB8C4E0901D1a1550aD2e64D568E69909",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0x8af398995b04c28e9951adb9721ef74c74f93e6a478f39e7e0777be13527e7ef",
+        subscriptionId: "7545",
+        callbackGasLimit: "500000",
+        interval: "30",
+    },
+    31337: {
+        name: "hardhat",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0x8af398995b04c28e9951adb9721ef74c74f93e6a478f39e7e0777be13527e7ef",
+        callbackGasLimit: "500000",
+        interval: "30",
+    },
+}
+
+const developmentChains = ["hardhat", "localhost"]
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+}
